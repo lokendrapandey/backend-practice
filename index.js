f39c9f51ef46fc35f5f1e07dea651bc00370480a
lokendra pandey <lokendrapandey5278@gmail.com>
2026-06-13 12:53:37 +0530

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log(history)
+
+const express = require('express');
+require('dotenv').config();
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+app.get('/shivam', (req,res)=>{
+    res.send("hello bhai")
+})
+app.get('/eweb', (req,res)=>{
+    res.send("<h2>working on project </h2>")
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port ${PORT}`)
+})
+// console.log("lokendra pandey");
